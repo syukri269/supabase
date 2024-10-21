7d216886d0dc75a45f866e4c5bca4b0cbc32b691
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-21 14:45:41 +0530

    user can now delete a publication

--- packages/api-types/types/api.d.ts
+++ packages/api-types/types/api.d.ts
@@ -1013,6 +1013,10 @@ export interface paths {
     /** Creates a publication */
     post: operations['ReplicationSourcesController_createPublication']
   }
+  '/platform/replication/{ref}/sources/{source_id}/publications/{publication_name}': {
+    /** Deletes a publication */
+    delete: operations['ReplicationSourcesController_deletePublication']
+  }
   '/platform/replication/{ref}/sources/{source_id}/tables': {
     /** Gets source tables */
     get: operations['ReplicationSourcesController_getTables']
@@ -2990,7 +2994,6 @@ export interface components {
       publish_update?: boolean
       tables?: string[] | null
     }
-    CreatePublicationResponse: Record<string, never>
     CreateReplicationPublicationBody: {
       /** @description Publication name */
       name: string
@@ -13403,11 +13406,31 @@ export interface operations {
     }
     responses: {
       201: {
-        content: {
-          'application/json': components['schemas']['CreatePublicationResponse']
-        }
+        content: never
       }
       /** @description Failed to create publication */
+      500: {
+        content: never
+      }
+    }
+  }
+  /** Deletes a publication */
+  ReplicationSourcesController_deletePublication: {
+    parameters: {
+      path: {
+        /** @description Project ref */
+        ref: string
+        /** @description Source id */
+        source_id: number
+        /** @description Publication name */
+        publication_name: string
+      }
+    }
+    responses: {
+      200: {
+        content: never
+      }
+      /** @description Failed to delete publication */
       500: {
         content: never
       }
